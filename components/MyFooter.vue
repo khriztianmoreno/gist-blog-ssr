f18e0966cce954b204364a6a29bd3ab853c79d87
khriztianmoreno <khriztianmoreno@gmail.com>
2017-11-03 04:13:36 -0500

    Add componentes Header and Footer

---
+++ components/MyFooter.vue
@@ -0,0 +1,40 @@
+<template>
+  <footer id="footer">
+    <div class="footer-copyright">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <p>
+              <a href="http://twitter.com/jsconfco">@jsconfco</a> ©
+              Copyright 2017. All Rights Reserved.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+
+<style lang="scss" scoped>
+  #footer {
+    font-size: 1.1em;
+    clear: both;
+    .logo {
+      display: block;
+      &.mb-xl {
+        margin-bottom: 12px;
+      }
+    }
+    .footer-copyright {
+      background-color: #35495f;
+      border-top: 5px solid #3c8070;
+      margin-top: 40px;
+      padding: 30px 0 10px;
+      p {
+        color: #ffffff;
+        margin: 0;
+        padding: 0;
+      }
+    }
+  }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,52 +1,29 @@
 <template>
-  <div>
-    <nuxt/>
+  <div class="body">
+    <div role="main" class="main">
+      <!-- header component -->
+      <my-header />
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <nuxt/>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- footer component -->
+    <my-footer />
   </div>
 </template>
 
-<style>
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script>
+  import MyHeader from '~/components/MyHeader.vue'
+  import MyFooter from '~/components/MyFooter.vue'
 
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+  export default {
+    components: {
+      MyHeader,
+      MyFooter
+    }
+  }
+</script>
